a8ed4b77d3b45cbb12a2fbdb338b797d65aa58d8
student_ait405 <izmakovmorphium@mail.ru>
2022-10-19 09:30:27 +0300

    Добавил доение и колоросы

--- app/src/main/java/com/example/javaoop/Cat.java
+++ app/src/main/java/com/example/javaoop/Cat.java
@@ -2,9 +2,10 @@ package com.example.javaoop;
 
 import android.util.Log;
 
-public class Cat {
+public class Cat extends Animal{
     int age;
     String name;
+
     String color;
     String breed;
     public Cat()  {
@@ -14,16 +15,14 @@ public class Cat {
         this.color = color;
         this.breed = breed;
     }
-
     public Cat(int age, String name)  {
         this.age = age;
         this.name = name;
-
     }
 
 
     public void talk(){
         Log.i("talk()", "Meow! Hi! My name is " + name +
-    ", and i`m " + age + " years old.");
+                ", and i`m " + age + " years old.");
     }
 }
---
+++ app/src/main/java/com/example/javaoop/Puma.java
@@ -0,0 +1,6 @@
+package com.example.javaoop;
+
+public class Puma extends Cat{
+
+
+}
